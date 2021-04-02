9f245fffce63eb4b2bc175e34b518f423b64f1c6
mmwaikar <manoj.waikar@intercax.com>
2021-04-02 15:45:53 +0530

    Add DbCreator.

---
+++ lib/daos/DbCreator.dart
@@ -0,0 +1,41 @@
+import 'dart:io';
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+
+class DbCreator {
+  static Database _db;
+  List<String> _createTableStatements;
+
+  DbCreator(this._createTableStatements);
+
+  List<String> get createTableStatements => _createTableStatements;
+
+  Future<Database> get db async {
+    if (_db != null) {
+      print("_db instance is not null");
+      return _db;
+    }
+
+    print("_db instance is null");
+    _db = await initDb();
+    return _db;
+  }
+
+  initDb() async {
+    print("_initializing db");
+
+    Directory documentDirectory = await getApplicationDocumentsDirectory();
+    String path = join(documentDirectory.path, "notodo_db.db");
+    var ourDb = await openDatabase(path, version: 1, onCreate: _onCreate);
+    return ourDb;
+  }
+
+  void _onCreate(Database db, int version) async {
+    // await db.execute("CREATE TABLE $tableName($pkColumn INTEGER PRIMARY KEY, $colNamesWithDbTypes)");
+    _createTableStatements.forEach((statement) async {
+      await db.execute(statement);
+    });
+    print("Tables are created");
+  }
+}
